bdf871f3b54668d3a3ee59d1f17e26b704a22501
Marian Basti <31198560+marianbasti@users.noreply.github.com>
2024-06-12 15:09:32 -0300

    [MOD] Método más simplificado

--- ComunicacionesBCRA.py
+++ ComunicacionesBCRA.py
@@ -14,28 +14,27 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 class ScrapperBCRA:
     base_url = "https://www.bcra.gob.ar/SistemasFinancierosYdePagos/Buscador_por_tipo.asp"
     
-    def __init__(self, tipo, max_retries=3):
-        self.tipo = tipo
+    def __init__(self, max_retries=3):
         self.pdf_urls = []
         self.max_retries = max_retries
 
-    def scrape(self, stop_at_existing=False):
+    def scrape_urls(self, tipo, stop_at_existing=False):
         page = 1
         params = {
-            'tipo': self.tipo,
+            'tipo': tipo,
             'tamanopagina': 50,
             'paginaabsoluta': 1
         }
         existing_urls = set()
         
         # Load existing URLs if the file exists
-        filename = f'BCRA_{self.tipo}.txt'
+        filename = f'BCRA_{tipo}.txt'
         if os.path.exists(filename):
             with open(filename, 'r') as file:
                 existing_urls = set(file.read().splitlines())
         
         while True:
-            logging.info(f"Scraping page {page} for tipo {self.tipo}")
+            logging.info(f"Scraping page {page} for tipo {.tipo}")
             response = requests.get(self.base_url, params=params, verify=False)
             
             if response.status_code != 200:
@@ -60,7 +59,7 @@ class ScrapperBCRA:
 
             params['paginaabsoluta'] += 1
             page += 1
-        logging.info(f"Scraped {len(self.pdf_urls)} new PDF URLs for tipo {self.tipo}.")
+        logging.info(f"Scraped {len(self.pdf_urls)} new PDF URLs for tipo {tipo}.")
 
     def save_to_file(self, filename):
         with open(filename, 'w') as file:
@@ -68,39 +67,12 @@ class ScrapperBCRA:
                 file.write(f"{url}\n")
         logging.info(f"Saved {len(self.pdf_urls)} PDF URLs to {filename}")
 
-    def save_pdfs(self):
-        if not os.path.exists(f'{self.tipo}'):
-            os.makedirs(f'{self.tipo}')
-
-        for url in self.pdf_urls:
-            pdf_filename = url.split('/')[-1]
-            pdf_path = f'{self.tipo}/{pdf_filename}'
-            logging.info(f"Downloading {url} to {pdf_path}")
-            retries = 0
-            while retries < self.max_retries:
-                try:
-                    response = requests.get(url.replace('..','https://www.bcra.gob.ar'), verify=False)
-                    if response.status_code == 200:
-                        with open(pdf_path, 'wb') as file:
-                            file.write(response.content)
-                        break
-                    else:
-                        retries += 1
-                        logging.warning(f"Failed to download {url}, retry {retries}/{self.max_retries}")
-                        sleep(2)
-                except requests.exceptions.RequestException as e:
-                    retries += 1
-                    logging.error(f"Request exception: {e}, retry {retries}/{self.max_retries}")
-                    sleep(2)
-            else:
-                logging.error(f"Failed to download {url} after {self.max_retries} retries")
-
-    def scrap_pdfs_naive(self):
-        if not os.path.exists(f'{self.tipo}'):
-            os.makedirs(f'{self.tipo}')
+    def scrape_pdfs_naive(self, tipo):
+        if not os.path.exists(f'{tipo}'):
+            os.makedirs(f'{tipo}')
         
         params = {
-            'tipo': self.tipo,
+            'tipo': tipo,
             'tamanopagina': 50,
             'paginaabsoluta': 1
         }
@@ -117,11 +89,11 @@ class ScrapperBCRA:
         retries = 0
         for n in range(int(latest), 0, -1):
                 try:
-                    file_path = f'{self.tipo}/{self.tipo}{str(n).zfill(4)}.pdf'
+                    file_path = f'{tipo}/{tipo}{str(n).zfill(4)}.pdf'
                     if os.path.exists(file_path):
                         logging.info(f"PDF {file_path} already exists, skipping...")
                         continue
-                    url = f'https://www.bcra.gob.ar/Pdfs/comytexord/{self.tipo}{str(n).zfill(4)}.pdf'
+                    url = f'https://www.bcra.gob.ar/Pdfs/comytexord/{tipo}{str(n).zfill(4)}.pdf'
                     response = requests.get(url, verify=False)
                     if response.status_code == 200:
                         with open(file_path, 'wb') as file:
@@ -138,8 +110,8 @@ class ScrapperBCRA:
 def main():
     tipos = ['A', 'B', 'C', 'P']
     for tipo in tipos:
-        scrapper = ScrapperBCRA(tipo)
-        scrapper.scrap_pdfs_naive()
+        scrapper = ScrapperBCRA()
+        scrapper.scrap_pdfs_naive(tipo)
 
 if __name__ == "__main__":
     main()
